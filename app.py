b20dd53fcc3d76342c4f20a5a86ab4cfb0802ba2
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-15 16:04:27 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -52,39 +52,20 @@ def upload():
             
             for row in csv_reader:
                 # Extract the values from the row
-                time = row[0]
-                latitude = float(row[1])
-                longitude = float(row[2])
-                depth = float(row[3])
-                mag = float(row[4])  # Updated field name
-                magType = row[5]
-                nst = float(row[6])
-                gap = float(row[7])
-                dmin = float(row[8])
-                rms = float(row[9])
-                net = row[10]
-                eid = row[11]
-                updated = row[12]
-                place = row[13]
-                typ = row[14]
-                horizontalError = float(row[15])
-                depthError = float(row[16])
-                magError = float(row[17])
-                magNst = float(row[18])
-                status = row[19]
-                locationSource = row[20]
-                magSource = row[21]
+                City = row[0]
+                State = row[1]
+                Population = int(row[2])
+                lat = float(row[3])
+                lon = float(row[4])  # Updated field name
 
                 # Execute the SQL INSERT statement
                 cursor.execute('''
-                    INSERT INTO all_month (
-                        time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, typ,
-                        horizontalError, depthError, magError, magNst, status, locationSource, magSource
+                    INSERT INTO city (
+                        City, State, Population, lat, lon
                     )
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    VALUES (?, ?, ?, ?, ?)
                 ''', (
-                    time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, typ,
-                    horizontalError, depthError, magError, magNst, status, locationSource, magSource
+                    City, State, Population, lat, lon
                 ))
 
         conn.commit()
@@ -96,12 +77,12 @@ def upload():
 
 @app.route('/search', methods=['POST'])
 def search():
-    magnitude = float(request.form['magnitude'])
+    city = request.form['city'])
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
     cursor.execute('''
-        SELECT * FROM all_month WHERE mag > ?
-    ''', (magnitude,))
+        SELECT * FROM city WHERE City= ?
+    ''', (city,))
     results = cursor.fetchall()
     conn.close()
     return render_template('results.html', results=results)
