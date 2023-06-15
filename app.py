efe2cc80649392d5d34998452ea5f2ad42c3f727
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-15 13:05:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,54 +5,45 @@ import pyodbc
 app = Flask(__name__)
 
 # Azure SQL Database configuration
-server = 'prathikhegde.database.windows.net'
+server = 'tcp:prathikhegde.database.windows.net,1433'
 database = 'ASSS2'
 username = 'prathikhegde'
 password = 'Tco7890$'
-driver = '{ODBC Driver 17 for SQL Server}'
+driver = '{ODBC Driver 18 for SQL Server}'
 
-connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-
+connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
 def create_table():
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
-    table_name = "all_month"
-    if not table_exists(cursor, table_name):
-        cursor.execute(f'''
-            CREATE TABLE {table_name} (
-                id INT IDENTITY(1,1) PRIMARY KEY,
-                time VARCHAR(50),
-                latitude FLOAT,
-                longitude FLOAT,
-                depth FLOAT,
-                mag FLOAT,
-                magType VARCHAR(50),
-                nst FLOAT,
-                gap FLOAT,
-                dmin FLOAT,
-                rms FLOAT,
-                net VARCHAR(50),
-                eid VARCHAR(50),
-                updated VARCHAR(50),
-                place VARCHAR(100),
-                typ VARCHAR(50),
-                horizontalError FLOAT,
-                depthError FLOAT,
-                magError FLOAT,
-                magNst FLOAT,
-                status VARCHAR(50),
-                locationSource VARCHAR(50),
-                magSource VARCHAR(50)
-            )
-        ''')
-        conn.commit()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS all_month (
+            id INT IDENTITY(1,1) PRIMARY KEY,
+            time VARCHAR(50),
+            latitude FLOAT,
+            longitude FLOAT,
+            depth FLOAT,
+            mag FLOAT,
+            magType VARCHAR(50),
+            nst FLOAT,
+            gap FLOAT,
+            dmin FLOAT,
+            rms FLOAT,
+            net VARCHAR(50),
+            eid VARCHAR(50),
+            updated VARCHAR(50),
+            place VARCHAR(100),
+            type VARCHAR(50),
+            horizontalError FLOAT,
+            depthError FLOAT,
+            magError FLOAT,
+            magNst FLOAT,
+            status VARCHAR(50),
+            locationSource VARCHAR(50),
+            magSource VARCHAR(50)
+        )
+    ''')
+    conn.commit()
     conn.close()
-
-def table_exists(cursor, table_name):
-    cursor.execute(f"SELECT 1 FROM sys.tables WHERE name = '{table_name}'")
-    return cursor.fetchone() is not None
-
-
 
 @app.route('/')
 def index():
@@ -61,64 +52,52 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    file_path = 'STATIC/all_month.csv'  # Set the correct file path
-
-    # Save the file to disk
-    file.save(file_path)
-
     if file:
         create_table()
         conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
-        
-        # Open the file in text mode with the appropriate encoding
-        with open(file_path, 'r', encoding='utf-8') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            next(csv_reader)  # Skip header row
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip header row
+        for row in csv_reader:
+            time = row[0]
+            latitude = float(row[1])
+            longitude = float(row[2])
+            depth = float(row[3])
+            mag = float(row[4])  # Updated field name
+            magType = row[5]
+            nst = float(row[6])
+            gap = float(row[7])
+            dmin = float(row[8])
+            rms = float(row[9])
+            net = row[10]
+            eid = row[11]
+            updated = row[12]
+            place = row[13]
+            typ = row[14]
+            horizontalError = float(row[15])
+            depthError = float(row[16])
+            magError = float(row[17])
+            magNst = float(row[18])
+            status = row[19]
+            locationSource = row[20]
+            magSource = row[21]
             
-            for row in csv_reader:
-                # Extract the values from the row
-                time = row[0]
-                latitude = float(row[1])
-                longitude = float(row[2])
-                depth = float(row[3])
-                mag = float(row[4])  # Updated field name
-                magType = row[5]
-                nst = float(row[6])
-                gap = float(row[7])
-                dmin = float(row[8])
-                rms = float(row[9])
-                net = row[10]
-                eid = row[11]
-                updated = row[12]
-                place = row[13]
-                typ = row[14]
-                horizontalError = float(row[15])
-                depthError = float(row[16])
-                magError = float(row[17])
-                magNst = float(row[18])
-                status = row[19]
-                locationSource = row[20]
-                magSource = row[21]
-
-                # Execute the SQL INSERT statement
-                cursor.execute('''
-                    INSERT INTO all_month (
-                        time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, typ,
-                        horizontalError, depthError, magError, magNst, status, locationSource, magSource
-                    )
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', (
-                    time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, typ,
-                    horizontalError, depthError, magError, magNst, status, locationSource, magSource
-                ))
-
+            cursor.execute('''
+                INSERT INTO all_month (
+                    time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, type, horizontalError,
+                    depthError, magError, magNst, status, locationSource, magSource
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                time, latitude, longitude, depth, mag, magType, nst, gap, dmin, rms, net, eid, updated, place, typ,
+                horizontalError, depthError, magError, magNst, status, locationSource, magSource
+            ))
+        
         conn.commit()
         conn.close()
         return 'Data imported successfully!'
-
+    
     return 'No file selected.'
-
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -135,5 +114,3 @@ def search():
 
 if __name__ == '__main__':
     app.run()
-
-    
